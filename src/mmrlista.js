82b0a8ca9fd390626f2129e25cad61d704447345
LaukkaE <erkkalaukkanen@gmail.com>
2023-01-26 13:48:52 +0200

    update google docs url

--- src/mmrlista.js
+++ src/mmrlista.js
@@ -4,12 +4,12 @@ let range = 'Vastauksista 1!B:D';
 let mmrlista = {};
 // const localMmrLista = require('../localmmrlist.json');
 
-// Tallentaa listan objectiin muodossa {nimi:[mmr,roolit]} 
+// Tallentaa listan objectiin muodossa {nimi:[mmr,roolit]}
 const getMmrList = async () => {
     try {
         // mmrlista = {};
         const response = await axios.get(
-            `https://sheets.googleapis.com/v4/spreadsheets/1iRYAQcRYoaovfTVYzpMNasQmd00r9542sMsJBdllLt0/values/${range}?key=${process.env.GOOGLE_SHEETS_API_KEY}`
+            `https://sheets.googleapis.com/v4/spreadsheets/1TlP5V9JCKHxBxtuPAIbsb4egNZyIEPj7a_1XYFFCEJQ/values/${range}?key=${process.env.GOOGLE_SHEETS_API_KEY}`
         );
         response.data.values.shift();
         response.data.values.forEach((e, i) => {
