5194bec59b8c84e1d166811a86b007e350b22ea3
LaukkaE <erkkalaukkanen@gmail.com>
2023-02-04 21:41:28 +0200

    lisätty vielä avg mmr tiimeille

--- src/webhookfunctions.js
+++ src/webhookfunctions.js
@@ -139,12 +139,12 @@ const webHookMmr = (data) => {
             fields: [
                 {
                     name: 'Radiant',
-                    value: '\u200B',
+                    value: `average MMR **${Math.round(radiantMmr / 5)}**`,
                     inline: true,
                 },
                 {
                     name: 'Dire',
-                    value: '\u200B',
+                    value: `average MMR **${Math.round(direMmr / 5)}**`,
                     inline: true,
                 },
                 {
