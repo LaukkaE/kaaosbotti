e870b531453a3a07ad2825acb07cdc6f02fa98c2
LaukkaE <erkkalaukkanen@gmail.com>
2023-02-04 19:02:55 +0200

    modify embed empty fields

--- src/webhookfunctions.js
+++ src/webhookfunctions.js
@@ -144,7 +144,8 @@ const webHookMmr = (data) => {
                     inline: true,
                 },
                 {
-                    name: 'empty',
+                    name: '\u200B',
+                    value: '\u200B',
                     inline: false,
                 },
                 {
@@ -158,7 +159,8 @@ const webHookMmr = (data) => {
                     inline: true,
                 },
                 {
-                    name: 'empty',
+                    name: '\u200B',
+                    value: '\u200B',
                     inline: false,
                 },
                 {
@@ -172,7 +174,8 @@ const webHookMmr = (data) => {
                     inline: true,
                 },
                 {
-                    name: 'empty',
+                    name: '\u200B',
+                    value: '\u200B',
                     inline: false,
                 },
                 {
@@ -186,7 +189,8 @@ const webHookMmr = (data) => {
                     inline: true,
                 },
                 {
-                    name: 'empty',
+                    name: '\u200B',
+                    value: '\u200B',
                     inline: false,
                 },
                 {
